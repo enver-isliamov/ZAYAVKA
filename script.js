ef08703e4359f2fba6725495542e993fbe29084c
enver-isliamov <enverphoto@gmail.com>
2024-07-03 23:26:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,35 @@
-document.getElementById('submitBtn').addEventListener('click', function() {
-    // Собираем данные с редактируемых полей
-    let clientName = document.querySelector('.client-name').textContent.trim();
-    let phone = document.querySelector('p:nth-child(2) span').textContent.trim();
-    let order = document.querySelector('p:nth-child(3) span').textContent.trim();
-    let price = document.querySelector('p:nth-child(4) span').textContent.trim();
-    let quantity = document.querySelector('p:nth-child(5) span').textContent.trim();
-    let availability = document.querySelector('p:nth-child(6) span').textContent.trim();
-    let startDate = document.querySelector('p:nth-child(7) span').textContent.trim();
-    let duration = document.querySelector('p:nth-child(8) span').textContent.trim();
-    let remindDate = document.querySelector('p:nth-child(9) span').textContent.trim();
-    let endDate = document.querySelector('p:nth-child(10) span').textContent.trim();
-    let storage = document.querySelector('p:nth-child(11) span').textContent.trim();
-    let cell = document.querySelector('p:nth-child(12) span').textContent.trim();
-    let orderAmount = document.querySelector('p:nth-child(13) span').textContent.trim();
-    let debt = document.querySelector('p:nth-child(14) span').textContent.trim();
-    let contract = document.querySelector('p:nth-child(15) span').textContent.trim();
-    let trafficSource = document.querySelector('p:nth-child(16) span').textContent.trim();
+function sendToTelegram() {
+            const phoneNumber = "+79780703665"; // здесь можно вставить номер телефона клиента
+            const telegramBotToken = "7134836219:AAFOKRDl_f7_nft2Q52UxXFx244Gpqs7DPs";
+            const chatId = "96609347";
+            const message = `
+                Имя клиента: Денис Грошев
+                Телефон: ${phoneNumber}
+                Заказ: ❱ Pirelli Scorpion ➽ 255/55/R16 ➽ Зима ➽ без дисков
+                Цена за месяц: 600
+                Кол-во шин: 4
+                Наличие дисков: Без дисков
+                Начало: 29.06.2024
+                Напомнить: 22.08.2024
+                Окончание: 29.08.2024
+                Склад хранения: ABD13
+                Сумма заказа: 1200
+                Долг: -
+                Договор: 240629
+                Статус: Активный
+                Источник трафика: ❰AVITO❱
+            `;
+            const encodedMessage = encodeURIComponent(message);
+            const telegramUrl = `https://api.telegram.org/bot${telegramBotToken}/sendMessage?chat_id=${chatId}&text=${encodedMessage}`;
+            
+            fetch(telegramUrl)
+                .then(response => response.json())
+                .then(data => {
+                    alert('Данные успешно отправлены в Telegram!');
+                })
+                .catch(error => {
+                    console.error('Ошибка при отправке данных в Telegram:', error);
+                    alert('Произошла ошибка при отправке данных в Telegram.');
+                });
+        }
     
-    // Формируем сообщение для отправки в Telegram
-    let message = `
-    Имя клиента: ${clientName}
-    Телефон: ${phone}
-    Заказ: ${order}
-    Цена за месяц: ${price} руб.
-    Количество шин: ${quantity}
-    Наличие дисков: ${availability}
-    Начало: ${startDate}
-    Срок: ${duration} месяца
-    Напомнить: ${remindDate}
-    Окончание: ${endDate}
-    Склад хранения: ${storage}
-    Ячейка: ${cell}
-    Сумма заказа: ${orderAmount} руб.
-    Долг: ${debt}
-    Договор: ${contract}
-    Источник трафика: ${trafficSource}
-    `;
-
-    // Отправка данных в Telegram бота
-    fetch(`https://api.telegram.org/bot<7134836219:AAFOKRDl_f7_nft2Q52UxXFx244Gpqs7DPs>/sendMessage?chat_id=<96609347>&text=${encodeURIComponent(message)}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Ошибка отправки данных в Telegram');
-            }
-            alert('Данные успешно отправлены в Telegram!');
-        })
-        .catch(error => {
-            console.error('Ошибка:', error);
-            alert('Произошла ошибка при отправке данных в Telegram.');
-        });
-});
